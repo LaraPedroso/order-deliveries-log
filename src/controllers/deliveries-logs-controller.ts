87a28ddfff0617d0326b7cb356b09602a4a62c0e
Lara <pedroso5@hotmail.com>
2025-07-24 12:44:28 -0300

    feat: implementar método de criação para logs de entrega com validação e verificação de status

--- src/controllers/deliveries-logs-controller.ts
+++ src/controllers/deliveries-logs-controller.ts
@@ -1,10 +1,33 @@
 import { Request, Response, NextFunction } from "express";
 import { prisma } from "@/database/prisma";
 import z from "zod";
+import { AppError } from "@/utils/AppError";
 
 class DeliveryLogsController {
     async create(request: Request, response: Response, next: NextFunction) {
-        return response.json({ message: "ok" });
+        const bodySchema = z.object({
+            delivery_id: z.string().uuid(),
+            description: z.string(),
+        });
+        const { delivery_id, description } = bodySchema.parse(request.body);
+
+        const delivery = await prisma.delivery.findUnique({
+            where: { id: delivery_id },
+        });
+
+        if (!delivery) {
+            throw new AppError("delivery not found", 404);
+        }
+
+        if (delivery.status === "processing") {
+            throw new AppError("change status to shipped");
+        }
+
+        await prisma.deliveryLog.create({
+            data: { deliveryId: delivery_id, description },
+        });
+
+        return response.status(201).json();
     }
 }
 
